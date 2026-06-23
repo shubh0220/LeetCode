829157192feb7ee830d5be1bead2b942e0a78b4f
Shubh Shukla <shuklashubh060@gmail.com>
2026-06-23 08:37:52 +0530

    Time: 0 ms (100%), Space: 48.1 MB (91.21%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length-1;
+        while(e>=s){
+          int  m =  (e+s)/2;
+            if(nums[m] == target){
+                return m;
+            }
+            else if(nums[m]<target){
+                s = m+1;
+            }
+            else{
+                e=m-1;
+            }
+        }
+        return -1;
+    }
+}
