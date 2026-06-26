0ea7a2788f31f2d0390c7ed8dbeda72b593cecec
Shubh Shukla <shuklashubh060@gmail.com>
2026-06-26 16:13:16 +0530

    Time: 0 ms (100%), Space: 42.5 MB (48.05%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    void helper(int i,int k,int n,List<List<Integer>> ans,List<Integer> list){
+        if(n < 0) return;
+        if(k == 0){
+            if(n == 0) ans.add(new ArrayList<>(list));
+            return;
+        }
+        if(i>9) return;
+        list.add(i);
+        helper(i+1,k-1,n-i,ans,list);
+        list.remove(list.size() -1);
+        helper(i+1,k,n,ans,list);
+    }
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        helper(1,k,n,ans,list);
+        return ans;
+    }
+}
