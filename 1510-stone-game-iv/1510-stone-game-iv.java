b448966157b99825a8b3572984d7c1c3e172050f
Shubh Shukla <shuklashubh060@gmail.com>
2026-08-09 23:30:02 +0530

    Time: 57 ms (21.69%), Space: 42.6 MB (61.11%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 0;
+        boolean[] dp = new boolean[n+1];
+        dp[0] = false;
         for(int j=1;j<=n;j++){
             for(int i=1;i*i<=j;i++){
-            if(dp[j-i*i] == 0) dp[j] =  1;
+            if(dp[j-i*i] == false) dp[j] =  true;
             }
         }
-        int k = dp[n];
-        if(k == 1) return true;
-        return false;
+        return dp[n];
     }
 }
