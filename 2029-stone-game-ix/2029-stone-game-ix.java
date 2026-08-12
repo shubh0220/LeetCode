b587b05a496725340b9d630268cce8ab70f6cdc8
Shubh Shukla <shuklashubh060@gmail.com>
2026-08-12 21:13:00 +0530

    Time: 4 ms (70.8%), Space: 114.9 MB (49.56%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int n = stones.length;
+        int[] cnt = new int[3];
+        for(int x : stones) cnt[x%3]++;
+        if(cnt[0]%2 == 0) return cnt[1]>0 && cnt[2]>0 ;
+        return Math.abs(cnt[1]-cnt[2]) >2;
+    }
+}
