bbe66ac7528373f1c4628350de0391cb23e229dd
Shubh Shukla <shuklashubh060@gmail.com>
2026-08-12 20:11:21 +0530

    Time: 4 ms (60.94%), Space: 92.2 MB (27.34%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -4,8 +4,8 @@ class Solution {
         int[] sum = new int[n+1];
         sum[0] = 0;
         for(int i=0;i<n;i++) sum[i+1] = sum[i] + stones[i];
-        int jatinLodu = sum[n];
-        for(int idx=n-1;idx>=2;idx--) jatinLodu = Math.max(sum[idx]-jatinLodu,jatinLodu);
-        return jatinLodu;
+        int ans = sum[n];
+        for(int idx=n-1;idx>=2;idx--) ans = Math.max(sum[idx]-ans,ans);
+        return ans;
     }
 }
