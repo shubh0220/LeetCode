0e7b436ae1b0541a9f5581489d352bf8ab4e251c
Shubh Shukla <shuklashubh060@gmail.com>
2026-08-19 23:05:34 +0530

    Time: 3 ms (86.39%), Space: 52.5 MB (5.14%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        if(n==1) return 1;
+        int[] candy = new int[n];
+        Arrays.fill(candy, 1);
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]) candy[i] = candy[i-1]+1;
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]) candy[i] = Math.max(candy[i],candy[i+1]+1);
+        }
+        int ans = 0;
+        for(int x : candy) ans += x;
+        return ans;
+    }
+}
