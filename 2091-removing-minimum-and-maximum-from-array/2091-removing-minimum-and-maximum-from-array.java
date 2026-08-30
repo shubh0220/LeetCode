0d3561a8076f03bdf4549ec7a0e6b77fd7ce6537
Shubh Shukla <shuklashubh060@gmail.com>
2026-08-30 07:50:11 +0530

    Time: 3 ms (61.82%), Space: 87 MB (8.73%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -11,8 +11,8 @@ class Solution {
         int c1 = Math.max(mn,mx) +1;
         int c2 = n - Math.min(mn,mx);
         int d1 = Math.min(Math.abs(mn-0),Math.abs(mx-0));
-        int d2 = Math.min(Math.abs(mn-(n-1)),Math.abs(mx-(n-1)));
-        int c3 = d1+d2+2;
+        int d2 = Math.min(Math.abs(mn-n),Math.abs(mx-n));
+        int c3 = d1+d2+1;
         return Math.min(c1,Math.min(c2,c3));
     }
 }
